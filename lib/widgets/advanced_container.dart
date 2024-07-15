4d88a649af4582aa177f24ac66b9190e978ea323
Jappe111 <jasper.mannonen@gmail.com>
2024-07-15 20:04:31 +0300

    fix padding

--- lib/widgets/advanced_container.dart
+++ lib/widgets/advanced_container.dart
@@ -127,66 +127,63 @@ class _AdvancedContainerState extends State<AdvancedContainer> {
 
     Widget container() {
       return Container(
-          width: widget.width,
-          height: widget.height,
-          decoration: widget.borderStyle == AdvancedContainerBorder.double
-              ? BoxDecoration(
-                  border: widget.borderStyle == AdvancedContainerBorder.double
-                      ? Border.all(
-                          color: Colors.black.withOpacity(0.7),
-                          width: 1.0,
-                          strokeAlign: BorderSide.strokeAlignOutside,
-                        )
-                      : null,
-                  borderRadius: widget.borderStyle == AdvancedContainerBorder.double ? BorderRadius.all(Radius.circular(widget.borderRadius)) : null,
-                  boxShadow: shadows,
-                )
-              : const BoxDecoration(),
-          child: Container(
-            padding: widget.padding,
-            decoration: BoxDecoration(
-              color: () {
-                switch (widget.background) {
-                  case AdvancedContainerBackground.addOnBackground:
-                    return (Theme.of(context).brightness == Brightness.light
-                        ? Colors.black.withOpacity(_bgColorDelta)
-                        : Colors.white.withOpacity(_bgColorDelta));
-                  case AdvancedContainerBackground.solidBackground:
-                    return Theme.of(context).colorScheme.background;
-                  case AdvancedContainerBackground.transparentBackground:
-                    return Theme.of(context).colorScheme.background.withOpacity(0.85);
-                  case AdvancedContainerBackground.custom:
-                    return widget.backgroundColor;
-                }
-              }(),
-              borderRadius: BorderRadius.all(Radius.circular(widget.borderRadius)),
-              border: () {
-                if (widget.borderStyle == AdvancedContainerBorder.single) {
-                  return Border.all(color: widget.borderColor ?? Theme.of(context).dividerColor, width: 1.0, strokeAlign: BorderSide.strokeAlignOutside);
-                } else if (widget.borderStyle == AdvancedContainerBorder.double) {
-                  return Border.all(
-                    color: widget.borderColor ?? (Theme.of(context).colorScheme.onInverseSurface.scale(lightness: 0.05)),
-                    width: 1.0,
-                    strokeAlign: BorderSide.strokeAlignInside,
-                  );
-                }
-
-                return null;
-              }(),
-              image: widget.blur ? const DecorationImage(
-                image: AssetImage(
-                  "resources/images/blur_noise.png",
-                  package: "shade_ui",
+        width: widget.width,
+        height: widget.height,
+        decoration: widget.borderStyle == AdvancedContainerBorder.double
+            ? BoxDecoration(
+                border: Border.all(
+                  color: Colors.black.withOpacity(0.7),
+                  width: 1.0,
+                  strokeAlign: BorderSide.strokeAlignOutside,
                 ),
-                fit: BoxFit.none,
-                repeat: ImageRepeat.repeat,
-                scale: 7,
-                opacity: 0.038,
-              ) : null,
-            ),
-            child: widget.child,
+                borderRadius: BorderRadius.circular(widget.borderRadius),
+                boxShadow: shadows,
+              )
+            : null,
+        child: Container(
+          decoration: BoxDecoration(
+            color: () {
+              switch (widget.background) {
+                case AdvancedContainerBackground.addOnBackground:
+                  return (Theme.of(context).brightness == Brightness.light
+                      ? Colors.black.withOpacity(_bgColorDelta)
+                      : Colors.white.withOpacity(_bgColorDelta));
+                case AdvancedContainerBackground.solidBackground:
+                  return Theme.of(context).colorScheme.background;
+                case AdvancedContainerBackground.transparentBackground:
+                  return Theme.of(context).colorScheme.background.withOpacity(0.85);
+                case AdvancedContainerBackground.custom:
+                  return widget.backgroundColor;
+              }
+            }(),
+            borderRadius: BorderRadius.circular(widget.borderRadius),
+            border: () {
+              if (widget.borderStyle == AdvancedContainerBorder.single) {
+                return Border.all(color: widget.borderColor ?? Theme.of(context).dividerColor, width: 1.0, strokeAlign: BorderSide.strokeAlignOutside);
+              } else if (widget.borderStyle == AdvancedContainerBorder.double) {
+                return Border.all(
+                  color: widget.borderColor ?? (Theme.of(context).colorScheme.onInverseSurface.scale(lightness: 0.05)),
+                  width: 1.0,
+                  strokeAlign: BorderSide.strokeAlignInside,
+                );
+              }
+
+              return null;
+            }(),
+            image: widget.blur ? const DecorationImage(
+              image: AssetImage(
+                "resources/images/blur_noise.png",
+                package: "shade_ui",
+              ),
+              fit: BoxFit.none,
+              repeat: ImageRepeat.repeat,
+              scale: 7,
+              opacity: 0.038,
+            ) : null,
           ),
-        );
+          child: widget.padding != null ? Padding(padding: widget.padding!, child: widget.child) : widget.child,
+        ),
+      );
     }
 
     return widget.blur ? ClipRRect(
