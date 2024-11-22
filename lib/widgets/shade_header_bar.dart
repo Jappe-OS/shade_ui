b8a338bdb033c9e67104dce4000cf4171c7452da
Jappe02 <jasper.mannonen@gmail.com>
2024-11-22 22:27:43 +0200

    Fixed ShadeHeaderBar and added ShadeWindowHeaderBar.

--- lib/widgets/shade_header_bar.dart
+++ lib/widgets/shade_header_bar.dart
@@ -21,14 +21,15 @@ import 'dart:async';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:shade_ui/shade_ui.dart';
+import 'package:yaru_window/yaru_window.dart';
 
 const _kHeaderBarHeroTag = '<shadeui headerbar hero tag>';
 const _kDefaultHeaderBarHeight = 35.0;
 const _kPopulatedHeaderbarHeight = 45.0;
 
 class ShadeHeaderBar extends StatefulWidget implements PreferredSizeWidget {
-  /// The primary title widget.
-  final Widget? title;
+  /// The title of the headerbar.
+  final String? title;
 
   /// A widget to display before the [title] widget.
   final Widget? leading;
@@ -143,7 +144,7 @@ class _ShadeHeaderBarState extends State<ShadeHeaderBar> {
     final backgroundColor = WidgetStateProperty.resolveAs(widget.backgroundColor, states) ?? defaultBackgroundColor.resolve(states);
     final foregroundColor = theme.colorScheme.onSurface;
 
-    final titleTextStyle = (theme.appBarTheme.titleTextStyle ?? theme.textTheme.titleLarge!).copyWith(
+    final titleTextStyle = (theme.textTheme.titleLarge ?? const TextStyle()).copyWith(
       color: foregroundColor,
       fontSize: 14,
       fontWeight: FontWeight.w500,
@@ -154,7 +155,7 @@ class _ShadeHeaderBarState extends State<ShadeHeaderBar> {
       color: light ? Colors.black.withOpacity(0.1) : Colors.white.withOpacity(0.06),
     );
     final border = Border(bottom: widget.backgroundColor == Colors.transparent ? BorderSide.none : defaultBorder);
-    final shape = border + (const Border());
+    //final shape = border + (const Border());
 
     const bSpacing = 0.0;
     const bPadding = EdgeInsets.only(left: BPPresets.small);
@@ -168,9 +169,7 @@ class _ShadeHeaderBarState extends State<ShadeHeaderBar> {
       );
     }
 
-    Widget maybeHero({
-      required Widget child,
-    }) {
+    Widget maybeHero({required Widget child}) {
       if (widget.heroTag == null || context.findAncestorWidgetOfExactType<Hero>() != null) {
         return child;
       }
@@ -191,6 +190,7 @@ class _ShadeHeaderBarState extends State<ShadeHeaderBar> {
     Widget buildWindowControls() => Padding(
       padding: bPadding,
       child: SpacedRow(
+        mainAxisSize: MainAxisSize.min,
         spacing: bSpacing,
         children: [
           if (widget.isMinimizable == true)
@@ -231,11 +231,10 @@ class _ShadeHeaderBarState extends State<ShadeHeaderBar> {
           PanGestureRecognizer: GestureRecognizerFactoryWithHandlers<PanGestureRecognizer>(
             PanGestureRecognizer.new,
             (instance) => instance
-              //..onStart = widget.isDraggable == true ? (_) => widget.onDrag?.call(context) : null
               ..gestureSettings = gestureSettings
               ..onDown = ((_) => widget.onDragStart?.call(context))
               ..onEnd = ((_) => widget.onDragEnd?.call(context))
-              ..onUpdate = ((p0) => widget.onDrag?.call(context, p0))
+              ..onUpdate = ((p0) => widget.onDrag?.call(context, p0)),
           ),
           _PassiveTapGestureRecognizer: GestureRecognizerFactoryWithHandlers<_PassiveTapGestureRecognizer>(
             _PassiveTapGestureRecognizer.new,
@@ -249,36 +248,44 @@ class _ShadeHeaderBarState extends State<ShadeHeaderBar> {
               ..gestureSettings = gestureSettings,
           ),
         },
-        child: AppBar(
-          elevation: 0,
-          automaticallyImplyLeading: false,
-          leading: backdropEffect(widget.leading),
-          title: backdropEffect(widget.title),
-          centerTitle: widget.centerTitle,
-          titleSpacing: widget.titleSpacing,
-          toolbarHeight: (widget.leading == null && widget.actions == null) ? _kDefaultHeaderBarHeight : _kPopulatedHeaderbarHeight,
-          foregroundColor: foregroundColor,
-          backgroundColor: backgroundColor,
-          titleTextStyle: titleTextStyle,
-          shape: shape,
-          actions: [
-            maybeHero(
-              child: backdropEffect(
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    ...?widget.actions,
-                    //
-                    if (widget.isMinimizable == true ||
-                        widget.isRestorable == true || 
-                        widget.isMaximizable == true || 
-                        widget.isClosable == true)
-                        buildWindowControls(),
-                  ],
+        child: Container(
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            border: border,
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: BPPresets.small),
+          height: widget.preferredSize.height,
+          child: Row(
+            mainAxisAlignment: widget.centerTitle == true ? MainAxisAlignment.center : MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              if (widget.leading != null) ...[
+                backdropEffect(widget.leading)!,
+                const SizedBox(width: 8),
+              ],
+              if (widget.title != null)
+                Expanded(
+                  child: backdropEffect(
+                    Text(
+                      widget.title!,
+                      style: titleTextStyle,
+                    ),
+                  )!,
                 ),
-              )!,
-            ),
-          ],
+              if (widget.actions != null || widget.isMinimizable == true || widget.isRestorable == true || widget.isMaximizable == true || widget.isClosable == true)
+                maybeHero(
+                  child: backdropEffect(
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        ...?widget.actions,
+                        buildWindowControls(),
+                      ],
+                    ),
+                  )!,
+                ),
+            ],
+          ),
         ),
       ),
     );
@@ -384,5 +391,138 @@ class _PassiveTapGestureRecognizer extends TapGestureRecognizer {
     super.handleTapCancel(down: down, cancel: cancel, reason: reason);
     _firstTapDown = null;
     _firstTapUp = null;
+  }
+}
+
+class ShadeWindowHeaderBar extends StatelessWidget implements PreferredSizeWidget {
+  const ShadeWindowHeaderBar({
+    super.key,
+    this.leading,
+    this.title,
+    this.actions,
+    this.centerTitle,
+    this.titleSpacing,
+    this.backgroundColor,
+    this.isActive,
+    this.isClosable,
+    this.isDraggable,
+    this.isMaximizable,
+    this.isMinimizable,
+    this.isRestorable,
+    this.onClose = YaruWindow.close,
+    this.onDrag = YaruWindow.drag,
+    this.onMaximize = YaruWindow.maximize,
+    this.onMinimize = YaruWindow.minimize,
+    this.onRestore = YaruWindow.restore,
+    this.onShowMenu = YaruWindow.showMenu,
+    this.heroTag = _kHeaderBarHeroTag,
+  });
+
+  /// The title of the headerbar.
+  final String? title;
+
+  /// A widget to display before the [title] widget.
+  final Widget? leading;
+
+  /// Widgets to display after the [title] widget.
+  final List<Widget>? actions;
+
+  /// Whether the title should be centered.
+  final bool? centerTitle;
+
+  /// Spacing around the title.
+  final double? titleSpacing;
+
+  /// The background color.
+  final Color? backgroundColor;
+
+  /// Whether the title bar visualized as active.
+  final bool? isActive;
+
+  /// Whether the title bar shows a close button.
+  final bool? isClosable;
+
+  /// Whether the title bar can be dragged to move the window.
+  final bool? isDraggable;
+
+  /// Whether the title bar shows a maximize button.
+  final bool? isMaximizable;
+
+  /// Whether the title bar shows a minimize button.
+  final bool? isMinimizable;
+
+  /// Whether the title bar shows a restore button.
+  final bool? isRestorable;
+
+  /// Called when the close button is pressed.
+  final FutureOr<void> Function(BuildContext)? onClose;
+
+  /// Called when the title bar is dragged to move the window.
+  final FutureOr<void> Function(BuildContext)? onDrag;
+
+  /// Called when the maximize button is pressed or the title bar is
+  /// double-clicked while the window is not maximized.
+  final FutureOr<void> Function(BuildContext)? onMaximize;
+
+  /// Called when the minimize button is pressed.
+  final FutureOr<void> Function(BuildContext)? onMinimize;
+
+  /// Called when the restore button is pressed or the title bar is
+  /// double-clicked while the window is maximized.
+  final FutureOr<void> Function(BuildContext)? onRestore;
+
+  /// Called when the secondary mouse button is pressed.
+  final FutureOr<void> Function(BuildContext)? onShowMenu;
+
+  /// The tag to use for the [Hero] wrapping the window controls.
+  ///
+  /// By default, a unique tag is used to ensure that the window controls stay
+  /// in place during page transitions. If set to `null`, no [Hero] will be used.
+  final Object? heroTag;
+
+  @override
+  Size get preferredSize => Size(0, (leading == null && actions == null) ? _kDefaultHeaderBarHeight : _kPopulatedHeaderbarHeight);
+
+  static Future<void> ensureInitialized() {
+    _windowStates.clear();
+    return YaruWindow.ensureInitialized().then((window) => window.hideTitle());
+  }
+
+  static final _windowStates = <YaruWindowInstance, YaruWindowState>{};
+
+  @override
+  Widget build(BuildContext context) {
+    final window = YaruWindow.of(context);
+    return StreamBuilder<YaruWindowState>(
+      stream: window.states(),
+      initialData: _windowStates[window],
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          _windowStates[window] = snapshot.data!;
+        }
+        final state = snapshot.data;
+        return ShadeHeaderBar(
+          leading: leading,
+          title: title ?? state?.title ?? '',
+          actions: actions,
+          centerTitle: centerTitle,
+          titleSpacing: titleSpacing ?? BPPresets.medium,
+          backgroundColor: backgroundColor,
+          isActive: isActive ?? state?.isActive,
+          isClosable: isClosable ?? state?.isClosable,
+          isDraggable: isDraggable ?? state?.isMovable,
+          isMaximizable: isMaximizable ?? state?.isMaximizable,
+          isMinimizable: isMinimizable ?? state?.isMinimizable,
+          isRestorable: isRestorable ?? state?.isRestorable,
+          onClose: onClose,
+          onDrag: (bc, _) => onDrag?.call(bc),
+          onMaximize: onMaximize,
+          onMinimize: onMinimize,
+          onRestore: onRestore,
+          onShowMenu: onShowMenu,
+          heroTag: heroTag,
+        );
+      },
+    );
   }
 }
